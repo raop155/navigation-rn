89064894d7cd8b6e7867503796f0657917106357
Ricardo Olarte <raop155@hotmail.com>
2021-01-05 10:59:09 -0500

    Add Navigation Params

--- App.js
+++ App.js
@@ -1,22 +1,43 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 
-const HomeScreen = () => {
+const HomeScreen = ({ navigation }) => {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app! Test</Text>
+      <Text>Open up App.js to start working on your app!</Text>
+      <Button title='Go to detail' onPress={() => navigation.navigate('Detail', { userId: 2 })} />
     </View>
   );
 };
 
-const AppNavigator = createStackNavigator({
-  Home: {
-    screen: HomeScreen,
+const DetailScreen = ({ navigation }) => {
+  const data = navigation.getParam('data', 'default value');
+
+  return (
+    <View style={styles.container}>
+      <Text>Detail Screen</Text>
+      <Text>{data}</Text>
+      <Button title='Go home' onPress={() => navigation.goBack()} />
+    </View>
+  );
+};
+
+const AppNavigator = createStackNavigator(
+  {
+    Home: {
+      screen: HomeScreen,
+    },
+    Detail: {
+      screen: DetailScreen,
+    },
   },
-});
+  {
+    initialRouteName: 'Home',
+  },
+);
 
 const AppContainer = createAppContainer(AppNavigator);
 
