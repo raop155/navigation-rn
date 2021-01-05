d2fbee41650f91bd0603bf0b81c18c620a78fc85
Ricardo Olarte <raop155@hotmail.com>
2021-01-05 10:31:36 -0500

    Add AppContainer & StackNavigator

--- App.js
+++ App.js
@@ -1,15 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
 
-export default function App() {
+const HomeScreen = () => {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Open up App.js to start working on your app! Test</Text>
     </View>
   );
-}
+};
+
+const AppNavigator = createStackNavigator({
+  Home: {
+    screen: HomeScreen,
+  },
+});
+
+const AppContainer = createAppContainer(AppNavigator);
+
+const App = () => {
+  return <AppContainer />;
+};
+
+export default App;
 
 const styles = StyleSheet.create({
   container: {
